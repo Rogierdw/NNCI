445707286d583a97cba49dea3c2fb0c3e7c56c20
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-12-19 14:10:14 +0100

    added last bonus again. (The trick is to clamp the inputs)

--- lab_files/lab2/main.m
+++ lab_files/lab2/main.m
@@ -117,3 +117,47 @@ title('Minover learning curve - N = 20, n_D = 10, n_{max} = 200')
 xlabel('\alpha (P/N)')
 ylabel('Generalization error')
 legend('lambda = 0.0', 'lambda = 0.1', 'lambda = 0.2', 'lambda = 0.3', 'lambda = 0.4', 'lambda = 0.5', 'Location', 'southwest')
+
+%% Iris dataset (extra bonus)
+clear all
+close all
+
+%preprocess the iris data for binary classification with minover
+load('fisheriris'); 
+data = meas(:,[3,4]); % here we just use the third and forth features  
+labels=grp2idx(species); 
+labels(labels==2)=-1; 
+labels(labels==3)=-1;
+
+%clamp the input with an extra dimension to find an inhomogeneous solution
+data = [data, ones(length(data), 1)];
+    
+nMax = 200;
+part = 30; % twenty percent of the data for testing
+% select train and test on randomly selected data with Monte Carlo cross validation
+cv = 10;
+data_ind = 1:length(data);
+pred_err = zeros(1, cv);
+for i = 1:cv
+    % draw indices randomly for the test data without replacement, and calculate
+    % the indices for the train data using the set difference
+    test_ind = datasample(data_ind,part,'Replace',false);
+    train_ind = setdiff(data_ind, test_ind);
+    
+    test_data = data(test_ind,:);
+    test_labels = labels(test_ind);
+    train_data = data(train_ind,:);
+    train_labels = labels(train_ind);
+    
+    w = minover(nMax, train_data, train_labels);
+    
+    for j = 1:part
+        test_vec = test_data(j,:);
+        prediction = sign(dot(w, test_vec));
+        pred_err(i) = pred_err(i) + ~(test_labels(j) == prediction);
+    end
+    pred_err(i) = pred_err(i)/part;
+end
+
+mean(pred_err)
+
