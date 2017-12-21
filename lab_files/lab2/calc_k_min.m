49d7c6ea3615d06897a34fe594eb3b512761c0fd
Rogier de Weert <rogier92@gmail.com>
2017-12-21 14:28:41 +0100

    bugfix

--- lab_files/lab2/calc_k_min.m
+++ lab_files/lab2/calc_k_min.m
@@ -1,6 +1,8 @@
 function [ K_min, dat_min, lab_min ] = calc_k_min( w, data, labels )
     % Minimum stability calculation
 K_min = dot(w,data(1,:)*labels(1));
+dat_min = data(1,:);
+lab_min = labels(1);
 for t = 2:size(data,1)
     K_v_t = dot(w,data(t,:)*labels(t)); % Stability calculation
     if K_v_t < K_min % Smallest stability search
